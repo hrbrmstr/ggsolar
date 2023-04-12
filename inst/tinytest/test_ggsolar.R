84d2c614d8893809a9209da757067ab90dd76e3a
hrbrmstr <bob@rud.is>
2023-04-12 05:38:29 -0400

    chore: added tests

--- inst/tinytest/test_ggsolar.R
+++ inst/tinytest/test_ggsolar.R
@@ -1,4 +1,20 @@
+library(ggsolar)
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+shld_be <- c("Lesra Viii", "Tinus Proxima", "Nigplu D", "Nusam", "Gussiania",
+  "Aenma", "Depe D", "Caty Proxima", "Magnus", "Cosamlos")
+
+set.seed(1)
+is_it <- generate_random_planets(10)
+
+expect_equal(shld_be, is_it)
+
+set.seed(2)
+orbs <- generate_orbits(is_it)
+
+expect_equal(digest::digest(orbs), "2d00d7711cf4872621b8bd10f5fc34e3")
+
+set.seed(3)
+ppos <- randomize_planet_positions(orbs)
+
+expect_equal(digest::digest(ppos), "b2ef805bc635e2af4d7709ad96414ce9")
 

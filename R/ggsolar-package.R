41e545388e61f0c3ade0af555eddb4e4c8979deb
hrbrmstr <bob@rud.is>
2023-04-12 01:47:15 -0400

    R package repo initialization complete

---
+++ R/ggsolar-package.R
@@ -0,0 +1,10 @@
+#' ...
+#' 
+#' @md
+#' @name ggsolar
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+# @ useDynLib ggsolar, .registration = TRUE
+"_PACKAGE"
